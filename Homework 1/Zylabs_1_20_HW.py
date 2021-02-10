4cc86ef026ba988b0d13cde191813328477daaaf
Lorenzo Gonzales <77902872+LGonzales930@users.noreply.github.com>
2021-02-09 22:38:44 -0600

    Add files via upload
    This is my 2nd Zylabs Homework assignment

---
+++ Homework 1/Zylabs_1_20_HW.py
@@ -0,0 +1,13 @@
+print('Enter integer:')
+user_num = int(input())
+squared = user_num * user_num
+cubed = user_num * user_num * user_num
+print('You entered:',user_num)
+print(user_num,'squared is',squared)
+print('And',user_num,'cubed is',cubed,'!!')
+print('Enter another integer:')
+new_user = int(input())
+total_num = user_num + new_user
+product_num = user_num * new_user
+print(user_num,'+',new_user,'is',total_num)
+print(user_num,'*',new_user,'is',product_num)
