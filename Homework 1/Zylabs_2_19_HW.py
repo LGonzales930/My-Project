52460715fe776ee9c521b37c1d184aa9e68abbef
Lorenzo Gonzales <77902872+LGonzales930@users.noreply.github.com>
2021-02-10 11:42:13 -0600

    Add files via upload
    This is my third Zylabs HomeWork Assignment

---
+++ Homework 1/Zylabs_2_19_HW.py
@@ -0,0 +1,31 @@
+# This is my third zylabs assignment
+print('Enter amount of lemon juice (in cups):')  # 2 cups integer number
+num_1 = int
+int(input())
+print('Enter amount of water (in cups):')  # 16 cups integer number
+num_2 = int
+int(input())
+print('Enter amount of agave nectar (in cups):')  # 2.5 cups float number
+num_3 = float
+float(input())
+serving_num = num_1 + num_2 + num_3
+serving_num = serving_num / 3
+print('How many servings does this make?', serving_num)  # This makes 6 servings
+print('Lemonade ingredients - yields', serving_num, 'servings')
+print(num_1, 'cups lemon juice')
+print(num_2, 'cups water')
+print(num_3, 'cups agave nectar')
+
+
+print('How many servings would you like to make:')
+int(input())
+serving_amount = int / 16
+print('Lemonade ingredients - yields', serving_amount, 'servings')
+print('cups lemon juice')
+print('cups water')
+print('cups agave nectar')
+
+print('Lemon ingredients - yields' 'servings')
+print('gallon(s) lemon juice')
+print('gallon(s) water')
+print('gallon(s) agave nectar')
