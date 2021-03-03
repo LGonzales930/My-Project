664c50a5fb81ddbe96eec328f6f0074497157670
Lorenzo Gonzales <77902872+LGonzales930@users.noreply.github.com>
2021-03-03 00:07:42 -0600

    Add files via upload
    This program modifies your password into a stronger one

---
+++ Homework 2/Zylabs_6_17_HW.py
@@ -0,0 +1,11 @@
+# Lorenzo Gonzales
+# ID:1934789
+# Password Modifier
+users_password = input()  # User is prompted to input password
+users_password = users_password.replace('i', '!')  # i is replaced with !
+users_password = users_password.replace('a', '@')  # a is replaced with @
+users_password = users_password.replace('m', 'M')  # m is replaced with M
+users_password = users_password.replace('o', '.')  # o is replaced with .
+users_password = users_password.replace('B', '8')  # B is replaced with 8
+print(users_password + 'q*s')  # q*s is added next to the string value
+
