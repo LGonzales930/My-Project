6795363122e184a78a9459ef5e156d0d7e236e9a
Lorenzo Gonzales <77902872+LGonzales930@users.noreply.github.com>
2021-03-31 23:42:39 -0500

    Update Zylabs_11_22_HW.py

--- Homework 3/Zylabs_11_22_HW.py
+++ Homework 3/Zylabs_11_22_HW.py
@@ -1,4 +1,5 @@
-word_collection = input()
+word_collection = input() # words are inputted into the file
 word_collection = word_collection.split()  # variable inputs are split to allow multiple entries
-for W_C in range(len(word_collection)):  # W_C stands for word count
+for W_C in range(len(word_collection)):  # W_C stands for word count, for condtion is used to check for multiples by observing length
+    # Using string methods such as count, the words are counted
     print(word_collection[W_C], word_collection.count(word_collection[W_C])) # Words and thier frequnceis are printed
