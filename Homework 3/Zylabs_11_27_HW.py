cc0a96651bc44d962874adeeab69f26f5d1f14bd
Lorenzo Gonzales <77902872+LGonzales930@users.noreply.github.com>
2021-03-31 22:49:14 -0500

    This file contains the zylabs assingment for 11_27

---
+++ Homework 3/Zylabs_11_27_HW.py
@@ -0,0 +1,16 @@
+# Lorenzo Gonzales
+# ID: 1934789
+# Zylabs 11.27 CIS 2348 LAB*: Program: Soccer team roster (Dictionaries)
+player_stats = {'Jersey numbers': 'Player ratings'} # Keys in this case are integer values between (0 and 99) and (0 and 9)
+Jersey_input = input('Enter:')
+Player_input = input('Enter')
+Jersey_input = input('Enter')
+Player_input = input('Enter')
+Jersey_input = input('Enter')
+Player_input = input('Enter')
+Jersey_input = input('Enter')
+Player_input = input('Enter')
+Jersey_input = input('Enter')
+Player_input = input('Enter')
+
+
