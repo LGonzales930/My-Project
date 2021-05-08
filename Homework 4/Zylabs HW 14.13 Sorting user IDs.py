a72432b78946c5ce6a5ae5e87826ce55927881f4
Lorenzo Gonzales <77902872+LGonzales930@users.noreply.github.com>
2021-05-08 11:43:45 -0500

    Add files via upload
    The Fourth Zylabs Assignment from HW 4

---
+++ Homework 4/Zylabs HW 14.13 Sorting user IDs.py
@@ -0,0 +1,25 @@
+# Lorenzo Gonzales
+# ID: 1934789
+# Zylabs HW 14.13 Sorting user IDs
+num_calls = 0
+
+def partitoin(user_ids, i, k):
+    midpoint = i (k - i) // 2
+    pivot = user_ids [midpoint]
+
+def quicksort(user_ids, i, k)
+
+if __name__ == "__main__":
+    user_ids = []
+    user_id = input()
+    while user_id != "-1":
+        user_ids.append(user_id)
+        user_id = input()
+        quicksort(user_ids, 0, len(user_ids) - 1)
+
+    print(num_calls)
+
+    for user_id in user_ids:
+        print(user_id)
+
+
