57bcc77b8937eab1f5ea1a04aee3c2771d9448cb
Lorenzo Gonzales <77902872+LGonzales930@users.noreply.github.com>
2021-05-08 11:41:56 -0500

    Add files via upload
    The third Zylabs assignment for HW 4

---
+++ Homework 4/Zylabs HW 14.11 Descending selection sort with output during execution.py
@@ -0,0 +1,19 @@
+# Lorenzo Gonzales
+# ID: 1934789
+# Zylabs HW 14.11 Descending selection sort with output during execution
+def selection_sort_descend_trace(numbers):  # The function select sort is created
+    int(input).split()  # input splits numbers
+    for i in range(len(numbers) - 1):
+        index_max = i
+        for j in range(i - 1, len(numbers)):
+            if numbers[j] > numbers[index_max]:
+                index_max = j
+
+
+
+
+
+if __name__ == "__main__":
+    numbers = []
+    numbers = int(input())
+    print(numbers)
